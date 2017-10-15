9dfc3512c55a597c26e20c1ba8ff213384cd7546
Gabriel Leibovich <gabeleibo@Gabriels-MacBook-Pro.local>
2017-10-15 17:53:29 +0200

    light-box

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,1 +1,27 @@
 //Js functions go here
+var fadeTime = 300;
+
+function close() {
+  $('.modal').fadeOut(fadeTime);
+  $('body').removeClass('no-scroll');
+  setTimeout(function() {
+    $('.modal').scrollTop(0);
+  }, fadeTime - 50);
+}
+$('.close').click(function(e) {
+  close();
+});
+
+$('.modal').click(function() {
+  close();
+});
+
+$('.modal-content').click(function(e) {
+  e.stopPropagation();
+});
+
+$('#open').click(function(e) {
+  e.preventDefault();
+  $('.modal').fadeIn(fadeTime);
+  $('body').addClass('no-scroll');
+});
